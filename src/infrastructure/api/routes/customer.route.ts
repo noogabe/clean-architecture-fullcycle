ad92f45f0227d615a6f375ed1e885b28ca597b1d
Gabriele Almeida <naogabe@gmail.com>
2024-01-17 13:14:05 -0300

    feat: :sparkles: create customer list endpoint

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
 
 export const customerRoute = express.Router();
 
@@ -17,6 +18,16 @@ customerRoute.post("/", async (req: Request, res: Response) => {
       },
     };
     const output = await usecase.execute(customerDto);
+    res.send(output);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListCustomerUseCase(new CustomerRepository());
+  try {
+    const output = await usecase.execute({});
     res.send(output);
   } catch (err) {
     res.status(500).send(err);
